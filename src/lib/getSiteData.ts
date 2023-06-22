a1d3f4b3b2b242ee0c74699f3717dbc6e234b020
SuttonJack <jack@momenthouse.com>
2023-06-21 20:37:14 -0700

    ∆

--- src/lib/getSiteData.ts
+++ src/lib/getSiteData.ts
@@ -4,7 +4,6 @@ import { cache } from 'react'
 
 export const getSiteData = cache(async (subdomain: string) => {
   const res = await fetch(`https://multi-tenant-api.vercel.app/api/getEmoji`, {
-    cache: process.env.VERCEL === '1' ? 'force-cache' : 'no-store',
     next: { tags: [subdomain] },
   })
   return res.json()
