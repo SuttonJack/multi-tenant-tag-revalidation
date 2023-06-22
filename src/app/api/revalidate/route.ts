8b9b0fd51709b0eb91a3f8329148d2fb38aa4d11
SuttonJack <jack@momenthouse.com>
2023-06-21 21:10:21 -0700

    ∆

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -5,10 +5,12 @@ export const runtime = 'edge'
 
 export function GET(req: NextRequest) {
   const subdomain = req.headers.get('host')?.split?.('.')?.[0]
-  const key = req.nextUrl.searchParams.get('key')
   try {
     revalidateTag(subdomain)
-    return NextResponse.json({ revalidated: true, now: Date.now() })
+    return NextResponse.json({
+      message: `Revalidated ${subdomain}.mmnt.li`,
+      now: Date.now(),
+    })
   } catch (e) {
     return NextResponse.json(e, { status: 500 })
   }
